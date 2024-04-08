aeb5c58307dcc6f30c61e6c3288ef8f798916b78
iyaTreesaBaby <diyababy1010@gmail.com>
2024-04-08 22:49:56 +0530

    NavBar completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,RouterProvider,Routes } from 'react-router-dom';
+import AllBus from './components/AllBus';
+import AddBus from './components/AddBus';
+import DeleteBus from './components/DeleteBus';
+import SearchBus from './components/SearchBus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<AllBus/>} />
+    <Route path="/Add" element={<AddBus/>} />
+    <Route path="/Delete" element={<DeleteBus/>} />
+    <Route path="/Search" element={<SearchBus/>} />
+  </Routes>
+
+  
+  </BrowserRouter>
+
   );
 }
 
