98890898808a087de2ab39069cee492c55b65bb5
iyaTreesaBaby <diyababy1010@gmail.com>
2024-04-08 22:41:56 +0530

    list completed

---
+++ src/components/AllBus.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+const AllBus = () => {
+  return (
+    <div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+<div className="row g-3">
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div class="card" >
+  <img height="350" src="https://www.simplytrip.in/images/volvo-bus-mobile-banner.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">1</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">BOOK NOW</a>
+  </div>
+</div>
+
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div class="card" >
+  <img height="350" src="https://www.simplytrip.in/images/volvo-bus-mobile-banner.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">2</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">BOOK NOW</a>
+  </div>
+</div>
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div class="card" >
+  <img height="350"src="https://www.simplytrip.in/images/volvo-bus-mobile-banner.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">3</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">BOOK NOW</a>
+  </div>
+</div>
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div class="card" >
+  <img height="350" src="https://www.simplytrip.in/images/volvo-bus-mobile-banner.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">4</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">BOOK NOW</a>
+  </div>
+</div>
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div class="card" >
+  <img height="350" src="https://www.simplytrip.in/images/volvo-bus-mobile-banner.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">5</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">BOOK NOW</a>
+  </div>
+</div>
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div class="card" >
+  <img height="350"src="https://www.simplytrip.in/images/volvo-bus-mobile-banner.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">6</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">BOOK NOW</a>
+  </div>
+</div>
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div class="card" >
+  <img height="350"src="https://www.simplytrip.in/images/volvo-bus-mobile-banner.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">7</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">BOOK NOW</a>
+  </div>
+</div>
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <div class="card" >
+  <img height="350"src="https://www.simplytrip.in/images/volvo-bus-mobile-banner.png" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">8</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">BOOK NOW</a>
+  </div>
+</div>
+    </div>
+</div>
+
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
+    </div>
+  )
+}
+
+export default AllBus
